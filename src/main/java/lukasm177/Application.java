4d3c90b5033ea7a388f82786b238273563fb00f3
lukasm177 <lum1988@web.de>
2016-09-27 11:49:03 +0200

    setup project

---
+++ src/main/java/lukasm177/Application.java
@@ -0,0 +1,27 @@
+package lukasm177;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+@RestController
+public class Application {
+	
+	@RequestMapping("/hello")
+	public String hello(){
+		return "hello";
+	}
+	
+   public static void main(String[] args) {
+       SpringApplication.run(Application.class, args);
+   }
+}
+
+
+
+
